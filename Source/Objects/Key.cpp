dd9f5f07cc306eb2a71723ad13e84b71ff0c5d8a
ads5699 <adamphillips@hotmail.fr>
2020-11-08 18:32:04 +0100

    Added Key

---
+++ Source/Objects/Key.cpp
@@ -0,0 +1,11 @@
+#include "Key.h"
+
+Key::Key(char identifier, int posx, int posy, Player& p, RenderContext& renderer) : Object(posx, posy, p, "key", renderer), id(identifier)
+{
+
+}
+
+char Key::getId()
+{
+  return id;
+}
--- Source/Objects/Key.h
+++ Source/Objects/Key.h
@@ -1,4 +1,17 @@
-class Key
-{
+#ifndef KEY_H
+#define KEY_H
 
+#include "Object.h"
+
+class Key : public Object
+{
+ private:
+  /*Each key has a unique character as an identifier, and the key can unlock a door iff they have the same identifier. */
+  char id;
+ public:
+  /*Constructor*/
+  Key(char identifier, int posx, int posy, Player& p, RenderContext& renderer);
+  char GetId();
 };
+
+#endif
