48e3c89ed26e105072170ba2b50cf6524248621e
vbonczak <vincent.bonczak@gmail.com>
2020-11-14 12:06:48 +0100

    Fix : Merge conflict

--- Source/Objects/Warp.cpp
+++ Source/Objects/Warp.cpp
@@ -5,15 +5,15 @@ Warp::~Warp()
 
 }
 
-Warp::Warp(int destMap, int destRoom, int destX, int destY, int posx, int posy, Player& p, RenderContext& renderer) :
-	Object(posx, posy, p, "empty", renderer), destMap(destMap), destRoom(destRoom), destX(destX), destY(destY)
+Warp::Warp(int destMap, int destRoom, int destX, int destY, int posx, int posy, string identifier, Player& p, RenderContext& renderer) :
+	Object(identifier, posx, posy, p, "empty", renderer), destMap(destMap), destRoom(destRoom), destX(destX), destY(destY)
 {
 
 }
 
 void Warp::updateObject(Player& p, RenderContext& renderer, EVENT_ARGS* ea)
 {
-	if (destMap == ea->currentMap)
+	if (destMap == currentMap)
 	{
 		*(ea->currentRoom) = destRoom;
 		p.teleport(destX, destY);
