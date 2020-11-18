d7d1f0882bd27a691ac25426b9db8d3a49ddc0ec
vbonczak <vincent.bonczak@gmail.com>
2020-11-18 22:03:43 +0100

    WIP : Interface for Monsters

--- Source/Characters/Monster.h
+++ Source/Characters/Monster.h
@@ -9,16 +9,18 @@
 #include "MovingEntity.h"
 #include "LivingEntity.h"
 
+using namespace std;
+
 class Monster : public MovingEntity, public LivingEntity
 {
 protected:
 	Texture* texture;
 public:
-	Monster(RenderContext& renderer);
+	Monster(RenderContext& renderer, string textureId);
 
 	virtual void render(RenderContext& renderer, int offsetX = 0, int offsetY = 0);
 	virtual void kill();
-	virtual void tick(int time);
+	virtual void tick(int time) = 0;
 };
 
 #endif
