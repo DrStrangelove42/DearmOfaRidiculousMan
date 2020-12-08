3872fd5095be8193a01a7811ca669e497eb3d51e
Quantum.L <36284306+DrStrangelove42@users.noreply.github.com>
2020-12-08 05:15:04 +0100

    Adding Astar.h

--- Source/Characters/MovementAlgos/Astar.h
+++ Source/Characters/MovementAlgos/Astar.h
@@ -6,7 +6,34 @@
 /// In this file we inplement the Astar algorithm for the monster movement
 /// </summary>
 
+using namespace std;
+class AStar
+	{
+	public:
+		AStar();
+		vector<Map> findPath(const Map& startPos, const Map& targetPos, HeuristicFunction heuristicFunc, int weight = 1);
+		void loadMap(const tring& fileName);
+		void setDiagonalMovement(bool enable);
 
+	private:
+		vector<Map> buildPath() const;
+		bool isValid(const Map& pos) const;
+		bool isBlocked(int index) const;
+		int convertTo1D(const Map& pos) const;
+
+		int m_weight;
+		int m_size;
+		uint m_nrOfDirections;
+		Map m_dimensions;
+		Map m_startPos;
+		Map m_targetPos;
+		priority_queue<Node> m_openList;
+		vector<bool> m_closedList;
+		vector<Node> m_cameFrom;
+		vector<int> m_grid;
+		vector<Map> m_directions;
+		HeuristicFunction m_heuristic;
+    }
 
 
 
