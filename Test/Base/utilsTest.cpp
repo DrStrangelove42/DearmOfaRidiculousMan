89963dfca89112f916f19b6d7c3b6e090cd883ae
vbonczak <vincent.bonczak@gmail.com>
2020-12-13 13:14:20 +0100

    Test : utils module

---
+++ Test/Base/utilsTest.cpp
@@ -0,0 +1,24 @@
+#include "utilsTest.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION(UtilsTest);
+
+void UtilsTest::TestRandRange(void) 
+{
+	CPPUNIT_ASSERT(GetRandom(15) < 15);
+	CPPUNIT_ASSERT(GetRandom(22) >= 0);
+}
+
+void UtilsTest::Textures(void) 
+{
+	CPPUNIT_ASSERT(textures.empty());
+}
+
+void UtilsTest::setUp()
+{
+	CPPUNIT_ASSERT(InitUtils() == 0);
+}
+
+void UtilsTest::tearDown()
+{
+	FreeTextures();
+}
---
+++ Test/Base/utilsTest.h
@@ -0,0 +1,21 @@
+#ifndef TEST_UTILS_H
+#define TEST_UTILS_H
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include "../../Source/Base/utils.h"
+
+class UtilsTest : public CppUnit::TestFixture {
+	CPPUNIT_TEST_SUITE(UtilsTest);
+	CPPUNIT_TEST(TestRandRange);
+	CPPUNIT_TEST(Textures);
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void TestRandRange(void);
+	void Textures(void);
+	virtual void setUp();
+	virtual void tearDown(); 
+};
+
+#endif
