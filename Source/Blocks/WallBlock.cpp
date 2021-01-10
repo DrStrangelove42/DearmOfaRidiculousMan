cae9f37dc8d616a30241c53f198407be8c02daa2
ads5699 <adamphillips@hotmail.fr>
2021-01-10 19:42:57 +0100

    Merged the two WallBlock constructors.

--- Source/Blocks/WallBlock.cpp
+++ Source/Blocks/WallBlock.cpp
@@ -1,10 +1,5 @@
 #include "WallBlock.h"
 
-WallBlock::WallBlock(int posx, int posy, RenderContext& renderer) : Block(posx, posy, "wall", renderer)
-{
-
-}
-
 WallBlock::WallBlock(int posx, int posy, RenderContext& renderer, string texture) : Block(posx, posy, texture, renderer)
 {
 
