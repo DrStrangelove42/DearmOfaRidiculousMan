5dd789b0c109f323bb4eb7c65ccaeb6e0e9fef68
ads5699 <adamphillips@hotmail.fr>
2021-01-10 19:35:21 +0100

    Merged the two FloorBlock constructors.

--- Source/Blocks/FloorBlock.cpp
+++ Source/Blocks/FloorBlock.cpp
@@ -1,10 +1,5 @@
 #include "FloorBlock.h"
 
-FloorBlock::FloorBlock(int posx, int posy, RenderContext& renderer) : Block(posx, posy, "floor", renderer, true)
-{
-
-}
-
 FloorBlock::FloorBlock(int posx, int posy, RenderContext& renderer, string texture) : Block(posx, posy, texture, renderer, true)
 {
 
