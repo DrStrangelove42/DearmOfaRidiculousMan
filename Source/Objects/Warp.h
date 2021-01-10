452e93bcbdbf606b3915cf6a76578406acf39390
ads5699 <adamphillips@hotmail.fr>
2021-01-10 18:43:50 +0100

    Added to the documentation.

--- Source/Objects/Warp.h
+++ Source/Objects/Warp.h
@@ -20,46 +20,40 @@ protected:
 	/// The map we are going to warp to. If destMap == -1, we return to the main menu map.
 	/// </summary>
 	int destMap;
+	
 	/// <summary>
 	/// The room we are going to warp to.
 	/// </summary>
 	int destRoom;
+	
 	/// <summary>
 	/// The x coordinate we are going to warp to.
 	/// </summary>
 	int destX;
+	
 	/// <summary>
 	/// The y coordinate we are going to warp to.
 	/// </summary>
 	int destY;
+	
 	/// <summary>
 	/// The delay, in ticks, before the warp is active (if for example we want the player to warp back to the main menu map after a certain amount of time).
 	/// </summary>
 	int delay;
+	
 	/// <summary>
 	/// If not empty, the warp is to be done toward a different world.
 	/// </summary>
 	string destWorld;
+	
 public:
-	/*Destructor*/
+	/// <summary>
+	/// Destructor.
+	/// </summary>
 	~Warp();
 
 	/// <summary>
-	/// Constructor
-	/// </summary>
-	/// <param name="destMap"></param>
-	/// <param name="destRoom"></param>
-	/// <param name="destX"></param>
-	/// <param name="destY"></param>
-	/// <param name="posx"></param>
-	/// <param name="posy"></param>
-	/// <param name="identifier"></param>
-	/// <param name="renderer"></param>
-	/// <param name="delay"></param>
-	Warp(int destMap, int destRoom, int destX, int destY, int posx, int posy, string identifier, RenderContext& renderer, int delay = 0);
-
-	/// <summary>
-	/// 
+	/// Constructor.
 	/// </summary>
 	/// <param name="destMap"></param>
 	/// <param name="destRoom"></param>
@@ -71,11 +65,11 @@ public:
 	/// <param name="renderer"></param>
 	/// <param name="delay"></param>
 	/// <param name="destWorld"></param>
-	Warp(int destMap, int destRoom, int destX, int destY, int posx, int posy, string identifier, RenderContext& renderer, int delay, string destWorld);
+	Warp(int destMap, int destRoom, int destX, int destY, int posx, int posy, string identifier, RenderContext& renderer, int delay = 0, string destWorld = "");
 
 
 	/// <summary>
-	/// Constructor used to deduce the members from a string when creating maps
+	/// Constructor used to deduce the members from a string when creating <see cref="Map">Maps</see>.
 	/// </summary>
 	/// <param name="headerline"></param>
 	/// <param name="posx"></param>
@@ -85,7 +79,7 @@ public:
 	Warp(string information, int posx, int posy,  RenderContext& renderer);
 	
 	/// <summary>
-	/// How does a warp interact with the player?
+	/// How does a warp interact with the Player?
 	/// </summary>
 	/// <param name="game"></param>
 	bool updateObject(GAME* game);
