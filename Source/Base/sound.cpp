4bc7b539b98b028771b417ccbf34b950f2013e03
vbonczak <vincent.bonczak@gmail.com>
2021-03-21 21:54:45 +0100

    Audio failures management and documentation

--- Source/Base/sound.cpp
+++ Source/Base/sound.cpp
@@ -2,19 +2,38 @@
 
 unordered_map<string, Mix_Music*> loadedMus;
 
+bool soundOk;
+
 void SoundInit()
 {
-	Mix_Init(MIX_INIT_MP3);
-	Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 512);
+	if (Mix_Init(MIX_INIT_MP3) == 0)
+	{
+		cout << "Mix_Init failed with : " << Mix_GetError() << endl <<
+			"Audio is therefore disabled" << endl;
+		soundOk = false;
+	}
+	else
+	{
+		if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 512) == -1)
+		{
+			cout << "OpenAudio failed with : " << Mix_GetError() << endl <<
+				"Audio is therefore disabled" << endl;
+			soundOk = false;
+		}
+		else
+			soundOk = true;
+	}
 }
 
 void SoundPlay(string soundId, int loops)
 {
+	if (!soundOk) return;
+
 	if (loadedMus.find(DATA_LOCATION + "Res/Audio/" + soundId + ".mp3") == loadedMus.end())
 	{
 		Mix_Music* music;
 		music = Mix_LoadMUS((DATA_LOCATION + "Res/Audio/" + soundId + ".mp3").c_str());
-		
+
 		loadedMus[soundId] = music;
 	}
 
@@ -23,6 +42,8 @@ void SoundPlay(string soundId, int loops)
 
 void SoundQuit()
 {
+	if (!soundOk) return;
+
 	Mix_HaltMusic();
 	for (auto& entry : loadedMus)
 	{
--- Source/Base/sound.h
+++ Source/Base/sound.h
@@ -5,13 +5,28 @@
 #include <string>
 #include "config.h"
 #include <unordered_map>
+#include <iostream>
 
 using namespace std;
 
+/// <summary>
+/// Initialises the audio interface.
+/// If this fails, the game goes on without sound (we do not consider
+/// audio failure as a breaking error).
+/// </summary>
 void SoundInit();
 
+/// <summary>
+/// Begin to play a song from the sound file given by the specified ID (id with MP3 extension added to it).
+/// So far only the MP3 format is supported by DOARM (seems sufficient).
+/// </summary>
+/// <param name="soundId"></param>
+/// <param name="loops">-1 by default (indefinitely).</param>
 void SoundPlay(string soundId, int loops = 1);
 
+/// <summary>
+/// Exits the audio system and frees memory
+/// </summary>
 void SoundQuit();
 
 #endif // !SOUND_H
